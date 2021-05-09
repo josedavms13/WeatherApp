39bbd449a07d4214c9adf14efe51325a7bd559f2
josedavms13 <josedav0ms13@gmail.com>
2021-05-08 21:51:55 -0500

    FEAT: --> Get location working. --> Fetch wather working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+
+    const [coords, changeCoords] = useState(null)
+
+    // const[]
+
+    // BEFORE Get user Localization
+    useEffect(()=>{
+
+         navigator.geolocation.getCurrentPosition((position)=>{changeCoords(position)}, (error)=>{changeCoords(error)})
+
+    },[])
+
+
+
+    // AFTER GETTING LOCALIZATION
+
+    useEffect(()=>{
+        if(coords === null) {
+            console.log('waiting');
+
+            // Show waiting card            console.log(coords);
+
+        }
+        else {
+            console.log(coords);
+
+            const latitude = coords.coords.latitude;
+            const longitude = coords.coords.longitude;
+            const apiKey = '8307e411e1e445f496120926210905';
+
+            console.log(latitude);
+            console.log(longitude);
+
+            const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&aqi=yes`;
+
+
+
+            //region FETCH WEATHER --------------------------------------
+
+
+            fetch(url, {
+                method : 'GET',
+            })
+                .then((response) => (response.json()))
+                .then(data => console.log(data));
+
+            // FETCH WEATHER
+            //endregion
+
+
+        }
+    },[coords])
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hola</h1>
       </header>
     </div>
   );
